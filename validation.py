8c0d97a2fceb1961330d756831f57469f431c9c6
RomanovIgnat <romanov.ignat.p@gmail.com>
2021-02-18 11:20:41 +0300

    fix

--- validation.py
+++ validation.py
@@ -9,4 +9,4 @@ import torchvision.transforms as transforms
 def PSNRmetrics(x, y, model):
     y_pred = model(x)
     mse = nn.MSELoss()((y_pred + 1) / 2, (y + 1) / 2)
-    return 10 * torch.log10(4.0 / mse)
+    return 10 * torch.log10(1 / mse)
